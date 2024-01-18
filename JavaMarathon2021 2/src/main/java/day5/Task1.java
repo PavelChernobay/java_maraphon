a4b253948c6f664db188d9dd296bdafd833c8aa3
Pavel Chernobay <mr.chernobay1990@gmail.com>
2024-01-18 18:36:43 +0300

    complete day5 tasks

--- JavaMarathon2021 2/src/main/java/day5/Task1.java
+++ JavaMarathon2021 2/src/main/java/day5/Task1.java
@@ -2,6 +2,43 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("Ford");
+        car.setColor("grey");
+        car.setYear(2013);
 
+        System.out.println("Марка автомобиля: " + car.getModel());
+        System.out.println("Цвет: " + car.getColor());
+        System.out.println("Год выпуска: " + car.getYear());
+    }
+}
+
+class Car {
+    private String model;
+    private String color;
+    private int year;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
