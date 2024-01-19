0ec636e15a503f715aa5221c2aa65a8d64ee943a
Pavel Chernobay <mr.chernobay1990@gmail.com>
2024-01-19 17:46:29 +0300

    change task day5

--- JavaMarathon2021 2/src/main/java/day5/Task2.java
+++ JavaMarathon2021 2/src/main/java/day5/Task2.java
@@ -1,12 +1,5 @@
 package day5;
 
-/**
- * 2. Создать класс Мотоцикл (англ. Motorbike) с полями “Модель”, “Цвет”, “Год выпуска”.
- * Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
- * Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
- * Вывести в консоль значение каждого из полей, используя get методы.
- */
-
 public class Task2 {
     public static void main(String[] args) {
         Motorbike motorbike = new Motorbike("Honda", "red", 1997);
@@ -14,29 +7,5 @@ public class Task2 {
         System.out.println("Марка мотоцикла: " + motorbike.getModel());
         System.out.println("Цвет: " + motorbike.getColor());
         System.out.println("Год выпуска: " + motorbike.getYear());
-    }
-}
-
-class Motorbike {
-    private String model;
-    private String color;
-    private int year;
-
-    public Motorbike(String model, String color, int year) {
-        this.model = model;
-        this.color = color;
-        this.year = year;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getYear() {
-        return year;
     }
 }
