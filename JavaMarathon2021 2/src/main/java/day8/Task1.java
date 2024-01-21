c22452296535d7c0950df355d49915d2b93f386d
Pavel Chernobay <mr.chernobay1990@gmail.com>
2024-01-21 19:58:13 +0300

    complete day8 tasks

--- JavaMarathon2021 2/src/main/java/day8/Task1.java
+++ JavaMarathon2021 2/src/main/java/day8/Task1.java
@@ -2,6 +2,32 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime1 = System.currentTimeMillis();
 
+        String string = "";
+
+        for (int i = 0; i < 20000; i++) {
+            string += i + " ";
+        }
+
+        System.out.println(string);
+
+        long stopTime1 = System.currentTimeMillis();
+
+        System.out.println(stopTime1 - startTime1);
+
+        long startTime2 = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 20000; i++) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+
+        long stopTime2 = System.currentTimeMillis();
+
+        System.out.println(stopTime2 - startTime2);
     }
 }
